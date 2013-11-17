6ea355553f78c263cabbef043d2a168089489063
William Tisäter <william@defunct.cc>
2013-11-17 23:13:30 +0100

    Make it HEAD only

--- libmaxminddb.rb
+++ libmaxminddb.rb
@@ -1,9 +1,8 @@
 require 'formula'
 
 class Libmaxminddb < Formula
-  homepage ''
-  head 'https://github.com/maxmind/libmaxminddb.git', :revision => "8299f68bd83da08a3204a1d8bb964ae2f39fdd66"
-  version '8299f68bd83da08a3204a1d8bb964ae2f39fdd66'
+  head 'https://github.com/maxmind/libmaxminddb.git'
+  homepage 'https://github.com/maxmind'
 
   depends_on 'automake' => :build
   depends_on 'libtool' => :build
